f4792931b209fce311f2ebfcdd91f97becb47156
CLAUDIO ALVES DE SANTANA <claudio.asantana@senacsp.edu.br>
2024-12-05 14:34:49 -0300

    innitial commit

---
+++ codigo.py
@@ -0,0 +1,13 @@
+#Passo 1: Entrar no sistema hhttps://dlp.hashtagtreinamentos
+#instalar o auto gui com o comando no terminal pip install pyautogui
+#Abrir o navegador e entrar no link
+#import pyautogui
+#import time
+#Passo 2: Fazer Login
+
+#Passo 3: Importar Banco de Dados
+# pip install pandas
+#import pandas
+#Passo 4: Cadastrar um produto no banco de dados
+
+#Passo 5: Repetir o processo de cadastro até acabar os produtoss.
